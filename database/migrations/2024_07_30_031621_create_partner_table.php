5f29c2694ee0dc68e98fe3246a5467a6d4459401
AndikaBastian <andikabastian20@gmail.com>
2024-07-30 10:20:54 +0700

    migration donasi, partner

---
+++ database/migrations/2024_07_30_031621_create_partner_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('partner', function (Blueprint $table) {
+            $table->integer('id_partner')->autoIncrement()->primary();
+            $table->string('partner_name');
+            $table->string('logo');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('partner');
+    }
+};
