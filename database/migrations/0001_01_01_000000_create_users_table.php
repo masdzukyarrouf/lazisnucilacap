312afe9ff332887524b35ded9629256bfcab296f
AndikaBastian <andikabastian20@gmail.com>
2024-07-30 11:06:20 +0700

    migrasi last_name, bikin login sama daftar

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->string('username');
             $table->string('password');
             $table->string('first_name');
+            $table->string('last_name');
             $table->string('no_telp');
         });
 
