c7e23091e3348b25ab04504448d78eeae37ff22f
AndikaBastian <andikabastian20@gmail.com>
2024-07-30 11:21:42 +0700

    logo lazisnu

--- resources/views/card_login.blade.php
+++ resources/views/card_login.blade.php
@@ -10,8 +10,8 @@
 <body>
     <div class="flex flex-col justify-center min-h-full px-6 py-12 lg:px-8">
         <div class="sm:mx-auto sm:w-full sm:max-w-sm">
-            <img class="w-auto h-10 mx-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company">
-            <h2 class="mt-10 text-2xl font-bold leading-9 tracking-tight text-center text-gray-900">MASUK</h2>
+            <img class="w-auto h-24 mx-auto" src="{{ asset('images/logo_lazisnu.png') }}" alt="Your Company">
+            <h2 class="mt-3 text-2xl font-bold leading-9 tracking-tight text-center text-gray-900">MASUK</h2>
             <p class="text-xl font-bold leading-9 tracking-tight text-center text-gray-900 ">Belum memiliki akun? <a href="http://" class="text-green-500">Daftar</a></p>
         </div>
             
