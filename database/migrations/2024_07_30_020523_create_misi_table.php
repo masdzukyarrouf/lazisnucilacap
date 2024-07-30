c360783a53d5615c5fa880d3054470ffbce71008
Mas Dzuky Ar-Rouf <127199885+Einkelberg@users.noreply.github.com>
2024-07-30 09:46:15 +0700

    first commit

---
+++ database/migrations/2024_07_30_020523_create_misi_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('misi', function (Blueprint $table) {
+            $table->integer('id_misi')->autoIncrement()->primary();
+            $table->text('misi');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('misi');
+    }
+};
---
+++ database/migrations/2024_07_30_021040_create_campaign_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('campaign', function (Blueprint $table) {
+            $table->integer('id_campaign')->autoIncrement()->primary();
+            $table->string('title');
+            $table->text('description');
+            $table->integer('goal');
+            $table->integer('raised');
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->integer('min_donation');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('campaign');
+    }
+};
