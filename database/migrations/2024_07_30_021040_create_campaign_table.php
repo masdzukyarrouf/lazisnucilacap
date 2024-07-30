cdaf027041dc59b41b0120efd8a1604cd4e0c30e
Mas Dzuky Ar-Rouf <127199885+Einkelberg@users.noreply.github.com>
2024-07-30 15:44:43 +0700

    tambah lokasi

--- database/migrations/2024_07_30_021040_create_campaign_table.php
+++ database/migrations/2024_07_30_021040_create_campaign_table.php
@@ -20,6 +20,7 @@ return new class extends Migration
             $table->date('start_date');
             $table->date('end_date');
             $table->integer('min_donation');
+            $table->string('lokasi');
         });
     }
 
