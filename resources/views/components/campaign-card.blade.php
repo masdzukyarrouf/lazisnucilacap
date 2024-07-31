f96b582835e5bf60451a1d117026d0989d6cf0d8
AndikaBastian <andikabastian20@gmail.com>
2024-07-31 14:30:35 +0700

    hadehh

--- resources/views/components/campaign-card.blade.php
+++ resources/views/components/campaign-card.blade.php
@@ -1,14 +1,14 @@
-<div class="bg-white shadow-lg rounded-lg overflow-hidden">
-    <img src="{{ asset('images/campaign_1.png') }}" alt="Picture" class="w-full object-cover">
+<div class="overflow-hidden bg-white rounded-lg shadow-lg">
+    <img src="{{ asset('images/campaign_1.png') }}" alt="Picture" class="object-cover w-full">
     <div class="p-4">
         <h2 class="text-xl font-semibold text-gray-800">Title 1</h2>
-        <p class="text-gray-600 mt-1">Location 1</p>
+        <p class="mt-1 text-gray-600">Location 1</p>
         <div class="mt-4">
-            <div class="bg-gray-200 rounded-full h-2">
-                <div class="bg-green-500 h-2 rounded-full" style="width: 60%;"></div>
+            <div class="h-2 bg-gray-200 rounded-full">
+                <div class="h-2 bg-green-500 rounded-full" style="width: 60%;"></div>
             </div>
-            <p class="mt-2 text-gray-700">6.000</p>
+            <p class="mt-2 text-gray-700">rp</p>
         </div>
-        <button class="mt-4 w-full bg-green-500 text-white py-2 rounded-lg hover:bg-green-600">Donate</button>
+        <button class="w-full py-2 mt-4 text-white bg-green-500 rounded-lg hover:bg-green-600">Donate</button>
     </div>
 </div>
