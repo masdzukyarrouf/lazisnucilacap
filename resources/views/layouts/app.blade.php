479a86f1d5333f57a8a9f121560b4fb4771858be
Mas Dzuky Ar-Rouf <127199885+Einkelberg@users.noreply.github.com>
2024-08-01 10:35:43 +0700

    footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -27,55 +27,72 @@
     </style>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap" rel="stylesheet">
 </head>
-    <body class="flex flex-col h-screen">
-        {{ $slot }}
-    </body>
-    <footer class="bg-gray-200 text-white py-8">
-        <div class="max-w-6xl mx-auto px-4">
-            <div class="flex flex-wrap ">
-                <!-- Column 1 -->
-                <div class="w-4/12  px-4 mb-4 md:mb-0">
-                    <img src="{{ asset('images/cooler_logo_lazisnu.png') }}" alt="" class="w-auto h-12">
-                    <p class="text-gray-700 text-sm mb-8">Lazisnucilacap.com adalah situs resmi Lembaga Amil Zakat, Infaq dan Shadaqah Nahdlatul Ulama (LAZISNU) Kabupaten Cilacap. Saran dan kritik : nucarelazisnukabupatencilacap@gmail.com</p>
-                    <ul class="flex space-x-4">
-                        <li><img src="{{ asset('images/phone.png') }}" alt=""></li>
-                        <li><img src="{{ asset('images/youtube.png') }}" alt=""></li>
-                        <li><img src="{{ asset('images/facebook.png') }}" alt=""></li>
-                        <li><img src="{{ asset('images/instagram.png') }}" alt=""></li>
-                    </ul>
+
+<body class="flex flex-col h-screen">
+    {{ $slot }}
+</body>
+<footer class="bg-gray-200 text-white py-8">
+    <div class="max-w-7xl mx-auto px-4">
+        <div class="flex flex-wrap ">
+            <!-- Column 1 -->
+            <div class="w-4/12   mb-4 md:mb-0">
+                <img src="{{ asset('images/cooler_logo_lazisnu.png') }}" alt="" class="w-auto h-12">
+                <p class="text-gray-700 text-sm mb-8">Lazisnucilacap.com adalah situs resmi Lembaga Amil Zakat, Infaq
+                    dan Shadaqah Nahdlatul Ulama (LAZISNU) Kabupaten Cilacap. Saran dan kritik :
+                    nucarelazisnukabupatencilacap@gmail.com</p>
+                <ul class="flex space-x-8">
+                    <li><img src="{{ asset('images/phone.png') }}" alt="" class="hover:scale-125"></li>
+                    <li><img src="{{ asset('images/youtube.png') }}" alt="" class="hover:scale-125"></li>
+                    <li><img src="{{ asset('images/facebook.png') }}" alt="" class="hover:scale-125"></li>
+                    <li><img src="{{ asset('images/instagram.png') }}" alt="" class="hover:scale-125"></li>
+                </ul>
+            </div>
+            <!-- Column 2 -->
+            <div class="w-2/12   mb-4 md:mb-0">
+                <h2 class="text-gray-800 text-xl font-semibold mb-4">Informasi</h2>
+                <ul class="">
+                    <li><a href="#" class="text-gray-800 hover:text-lg text-sm">Lazisnu Cilacap</a></li>
+                    <li><a href="#" class="text-gray-800 hover:text-lg text-sm">Pilar & Program</a></li>
+                    <li><a href="#" class="text-gray-800 hover:text-lg text-sm">Layanan</a></li>
+                    <li><a href="#" class="text-gray-800 hover:text-lg text-sm">Ziswaf</a></li>
+                    <li><a href="#" class="text-gray-800 hover:text-lg text-sm">Campaign</a></li>
+                    <li><a href="#" class="text-gray-800 hover:text-lg text-sm">Berita</a></li>
+                </ul>
+            </div>
+            <!-- Column 3 -->
+            <div class="w-3/12   mb-4 md:mb-0">
+                <h2 class="text-lg text-gray-800 font-semibold mb-4">Alamat</h2>
+                <p class="text-gray-700 text-sm mb-4">Jl. Masjid No. 09 Sidanegara
+                    <br>
+                    Cilacap Tengah-Cilacap
+                    <br>
+                    Kode pos : 53223
+                    <br>
+                    Telp : (0282) 539 5195
+                    <br>
+                    Hp/WA : 081228221010
+                </p>
+
+                <div class="">
+                    <button class="rounded-md text-white bg-green-500 px-4 py-2 ">Berita Lainnya</button>
                 </div>
-                <!-- Column 2 -->
-                <div class="w-2/12  px-4 mb-4 md:mb-0">
-                    <h2 class="text-lg font-semibold mb-4">Column 2</h2>
-                    <ul>
-                        <li><a href="#" class="text-gray-400 hover:text-white">Link 1</a></li>
-                        <li><a href="#" class="text-gray-400 hover:text-white">Link 2</a></li>
-                        <li><a href="#" class="text-gray-400 hover:text-white">Link 3</a></li>
-                        <li><a href="#" class="text-gray-400 hover:text-white">Link 4</a></li>
-                    </ul>
-                </div>
-                <!-- Column 3 -->
-                <div class="w-2/12  px-4 mb-4 md:mb-0">
-                    <h2 class="text-lg font-semibold mb-4">Column 3</h2>
-                    <ul>
-                        <li><a href="#" class="text-gray-400 hover:text-white">Link 1</a></li>
-                        <li><a href="#" class="text-gray-400 hover:text-white">Link 2</a></li>
-                        <li><a href="#" class="text-gray-400 hover:text-white">Link 3</a></li>
-                        <li><a href="#" class="text-gray-400 hover:text-white">Link 4</a></li>
-                    </ul>
-                </div>
-                <!-- Column 4 -->
-                <div class="w-4/12  px-4">
-                    <h2 class="text-lg font-semibold mb-4">Column 4</h2>
-                    <ul>
-                        <li><a href="#" class="text-gray-400 hover:text-white">Link 1</a></li>
-                        <li><a href="#" class="text-gray-400 hover:text-white">Link 2</a></li>
-                        <li><a href="#" class="text-gray-400 hover:text-white">Link 3</a></li>
-                        <li><a href="#" class="text-gray-400 hover:text-white">Link 4</a></li>
-                    </ul>
+
+            </div>
+            <!-- Column 4 -->
+            <div class="w-3/12 mb-4 md:mb-0">
+                <h2 class="text-lg text-gray-800 font-semibold mb-4">NU Care-Lazisnu Cilacap</h2>
+                <div class="w-40 bg-white h-40 rounded-lg">
+                    map
                 </div>
             </div>
+            <div class="w-full h-px bg-gray-300 mt-8 ">
+            </div>
+            <div class="w-full mt-4">
+                <h2 class="text-sm text-gray-600 text-center">Copyright © 2024 - NU Care Lazisnu Cilacap</h2>
+            </div>
+
         </div>
-    </footer>
+    </div>
+</footer>
 
 </html>
