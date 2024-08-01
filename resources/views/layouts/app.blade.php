719ffed1bbfddf3d21417e01a1664c7e3b91499e
Mas Dzuky Ar-Rouf <127199885+Einkelberg@users.noreply.github.com>
2024-08-01 13:25:11 +0700

    title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>NU Cilacap</title>
+    {{-- kirim title lewat class, default app name di env--}}
+    <title>{{ str_replace('_', ' ', $title ?? config('app.name')) }}</title>
+
     @vite('resources/css/app.css')
     <style>
         .sidebar {
