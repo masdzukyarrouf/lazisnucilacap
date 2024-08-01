e2ce56a54d183d7f66f0ecdbcbc8a75dc58b103c
AndikaBastian <andikabastian20@gmail.com>
2024-08-01 11:04:54 +0700

    bikin detail berita

---
+++ resources/views/livewire/berita.blade.php
@@ -0,0 +1,64 @@
+<x-navbar></x-navbar>
+    <div class="flex items-center justify-center px-6 py-12 lg:px-8">
+        <div class="p-8 mx-5 space-y-8 bg-white rounded-lg shadow-md"
+            style="background-image: url('{{ asset('images/bg_campaign.png') }}'); background-repeat: no-repeat; background-position: bottom right; height: 2000px; width: 1449px;">
+            <div class="flex flex-col justify-between pl-6">
+                <p class="text-2xl font-extrabold text-green-600 ">
+                    Berkat bantuan kaka baik
+                </p>
+                <p class="text-xl font-semibold text-black">
+                    Bantuan kaka baik membawa senyum bahagia untuk mereka.
+                </p>
+            </div>
+            <div class="grid grid-cols-1 gap-6 pt-1 mx-10 sm:grid-cols-2 lg:grid-cols-3">
+                <x-berita-card></x-berita-card>
+                <div>
+                    <x-berita-card></x-berita-card>
+                </div>
+                <div>
+                    <x-berita-card></x-berita-card>
+                </div>
+                <div>
+                    <x-berita-card></x-berita-card>
+                </div>
+                <div>
+                    <x-berita-card></x-berita-card>
+                </div>
+                <div>
+                    <x-berita-card></x-berita-card>
+                </div>
+                <div>
+                    <x-berita-card></x-berita-card>
+                </div>
+                <div>
+                    <x-berita-card></x-berita-card>
+                </div>
+                <div>
+                    <x-berita-card></x-berita-card>
+                </div>
+                <div>
+                    <x-berita-card></x-berita-card>
+                </div>
+                <div>
+                    <x-berita-card></x-berita-card>
+                </div>
+                <div>
+                    <x-berita-card></x-berita-card>
+                </div>
+            </div>
+            <div class="flex justify-center">
+                <div class="flex items-center space-x-2">
+                <button class="flex items-center justify-center w-10 h-10 text-gray-700 bg-gray-200 rounded-full hover:bg-gray-300 disabled:opacity-50" disabled>
+                    &lt;
+                </button>
+                
+                <button class="flex items-center justify-center w-10 h-10 text-white bg-green-600 rounded-full">1</button>
+                <button class="flex items-center justify-center w-10 h-10 text-gray-700 bg-gray-200 rounded-full hover:bg-gray-300">2</button>
+                
+                <button class="flex items-center justify-center w-10 h-10 text-gray-700 bg-gray-200 rounded-full hover:bg-gray-300">
+                    &gt;
+                </button>
+            </div>
+            </div>
+        </div>
+    </div>
