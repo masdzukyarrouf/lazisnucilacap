6d0fa0d32d2207d03a9f76cf4f00dc2124b00f17
Mas Dzuky Ar-Rouf <127199885+Einkelberg@users.noreply.github.com>
2024-08-01 14:54:16 +0700

    Update navbar.blade.php

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -4,7 +4,7 @@
             <img src="{{ asset('images/cooler_logo_lazisnu.png') }}" alt="" class="w-auto h-8">
         </a>
         <div class="hidden sm:flex block inline">
-            <x-nav-link title="Lazisnu Cilacap" url="#" :links="[
+            <x-nav-link title="Lazisnu Cilacap"  :links="[
                 ['href' => '#', 'text' => 'Visi & Misi'],
                 ['href' => '#', 'text' => 'Profil & Jajaran Pengurus'],
                 ['href' => '#', 'text' => 'Sejarah'],
@@ -14,7 +14,7 @@
                 ['href' => '#', 'text' => 'Mitra'],
             ]" isDropdown="true" />
 
-            <x-nav-link title="Pilar & Program" url="#" :links="[
+            <x-nav-link title="Pilar & Program"  :links="[
                 ['href' => '#', 'text' => 'NU Care Berdaya (Ekonomi)'],
                 ['href' => '#', 'text' => 'NU Care Cerdas (Pendidikan)'],
                 ['href' => '#', 'text' => 'NU Care Sehat (Kesehatan)'],
@@ -22,14 +22,14 @@
                 ['href' => '#', 'text' => 'NU Care Hijau (Lingkungan Hidup & Kebencanaan)'],
             ]" isDropdown="true" />
 
-            <x-nav-link title="Layanan" url="#" :links="[
+            <x-nav-link title="Layanan"  :links="[
                 ['href' => '#', 'text' => 'Konfirmasi Donasi'],
                 ['href' => '#', 'text' => 'Pengajuan Umum'],
                 ['href' => '#', 'text' => 'Layanan Mobisnu'],
                 ['href' => '#', 'text' => 'Gocap'],
             ]" isDropdown="true" />
 
-            <x-nav-link title="Ziswaf" url="#" :links="[
+            <x-nav-link title="Ziswaf"  :links="[
                 ['href' => '#', 'text' => 'Zakat'],
                 ['href' => '#', 'text' => 'Wakaf'],
                 ['href' => '#', 'text' => 'Fidyah'],
@@ -41,7 +41,7 @@
 
             <x-nav-link title="Berita" url="/berita" />
 
-            <div class="hidden space-x-4 sm:flex md:hidden">
+            <div class="block space-x-4 sm:flex">
                 <div class="relative group">
                     <a href="/login"
                         class="block px-6 py-2 text-white bg-green-600 border rounded-xl hover:border-green-600 hover:bg-white hover:text-green-600">Masuk</a>
