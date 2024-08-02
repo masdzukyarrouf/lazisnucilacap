475b33563c7e29c02123be74860483b295806132
Mas Dzuky Ar-Rouf <127199885+Einkelberg@users.noreply.github.com>
2024-08-02 12:48:46 +0700

    Update navbar.blade.php

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,9 +1,12 @@
-<nav class="sticky z-10 p-4 bg-white">
-    <div class="container flex items-center justify-between px-20 mx-8">
-        <a href="/">
-            <img src="{{ asset('images/cooler_logo_lazisnu.png') }}" alt="" class="w-auto h-8">
+<nav class="sticky top-0 z-10 p-4 bg-white shadow-md">
+    <div class="container mx-auto flex items-center justify-between px-8">
+        <!-- Logo -->
+        <a href="/" class="flex-shrink-0">
+            <img src="{{ asset('images/cooler_logo_lazisnu.png') }}" alt="Logo" class="w-auto h-8">
         </a>
-        <div class="hidden block inline sm:flex">
+        
+        <!-- Navigation Links -->
+        <div class="hidden md:flex space-x-4 items-center">
             <x-nav-link title="Lazisnu Cilacap"  :links="[
                 ['href' => '#', 'text' => 'Visi & Misi'],
                 ['href' => '#', 'text' => 'Profil & Jajaran Pengurus'],
@@ -36,29 +39,23 @@
                 ['href' => '#', 'text' => 'Qurban'],
             ]" isDropdown="true" />
 
-
             <x-nav-link title="Campaign" url="/campaign" />
-
             <x-nav-link title="Berita" url="/berita" />
 
-            <div class="block space-x-4 sm:flex">
-                <div class="relative group">
-                    <a href="/login"
-                        class="block px-6 py-2 text-white bg-green-600 border rounded-xl hover:border-green-600 hover:bg-white hover:text-green-600">Masuk</a>
-                </div>
-                <div class="relative group">
-                    <a href="/daftar"
-                        class="block px-6 py-2 text-green-600 border rounded-xl border-green-6 00 hover:bg-green-600 hover:text-white">Daftar</a>
-                </div>
-            </div>
-            <div class="flex items-center sm:hidden">
-                <button id="menu-btn" class="text-gray-300 hover:text-black focus:outline-none">
-                    <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24"
-                        xmlns="http://www.w3.org/2000/svg">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                            d="M4 6h16M4 12h16M4 18h16"></path>
-                    </svg>
-                </button>
+            <div class="flex space-x-4">
+                <a href="/login" class="px-6 py-2 text-white bg-green-600 border rounded-xl hover:border-green-600 hover:bg-white hover:text-green-600">Masuk</a>
+                <a href="/daftar" class="px-6 py-2 text-green-600 border rounded-xl border-green-600 hover:bg-green-600 hover:text-white">Daftar</a>
             </div>
         </div>
+
+        <!-- Mobile Menu Button -->
+        <div class="md:hidden flex items-center">
+            <button id="menu-btn" class="text-gray-300 hover:text-black focus:outline-none">
+                <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path>
+                </svg>
+            </button>
+        </div>
+    </div>
 </nav>
+
