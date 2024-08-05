49263252bc441e2269ecba45bd9f376dcfbb72e7
AndikaBastian <andikabastian20@gmail.com>
2024-08-05 14:46:16 +0700

    ganti icon

--- resources/views/livewire/daftar.blade.php
+++ resources/views/livewire/daftar.blade.php
@@ -12,6 +12,7 @@
     <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
         <form wire:submit.prevent='simpan'>
         <div class="space-y-6">
+        <div>
             <label for="username" class="block text-sm font-medium leading-6 text-gray-900">Username</label>
             <div class="mt-2">
             <input id="username" name="username" type="text" autocomplete="username" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
@@ -48,6 +49,7 @@
 
         <div>
             <button type="submit" class="flex w-full justify-center rounded-md bg-green-500 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-green-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-500">Daftar</button>
+        </div>
         </div>
         </form>
     </div>
--- resources/views/livewire/landing.blade.php
+++ resources/views/livewire/landing.blade.php
@@ -13,69 +13,130 @@
         {{-- <img src="{{ asset('images/arrowX.png') }}" alt="Arrow Right" class="absolute right-0 object-cover w-8 h-8 transform -scale-x-100"> --}}
     </div>
 
-    <div class="relative z-12 w-screen flex flex-wrap items-center justify-center">
-        <div
-            class="relative flex flex-wrap items-center justify-center h-auto py-10 md:mx-20 -mt-24 md:-mt-32 space-x-2 md:space-x-16 rounded-3xl">
-
+    <div class="relative z-12">
+        <div class="relative flex flex-wrap items-center justify-center h-auto py-10 mx-20 -mt-32 space-x-16 rounded-3xl">
+            
             <!-- Wrapper for Left Ornament and Item 1 -->
             <div class="relative flex items-center">
-                <img src="{{ asset('images/fknleaf.png') }}" alt="Left Ornament"
-                    class="absolute z-0 object-cover w-16 h-auto md:w-auto md:h-auto mb-2 transform -translate-y-1/2 -scale-x-100 -scale-y-100 -left-8 md:-left-28 top-1/2">
-
-                <div
-                    class="relative flex flex-col items-center w-24 h-28 p-4 md:w-auto md:h-auto md:p-8 bg-white rounded-lg shadow-2xl">
-                    <img src="{{ asset('images/talk.png') }}" alt="Image 1"
-                        class="relative object-cover w-8 h-8 md:w-24 md:h-24 mb-2">
-                    <p class="relative text-sm md:text-lg text-gray-800 text-center">Konsultasi</p>
+                <img src="{{ asset('images/fknleaf.png') }}" alt="Left Ornament" class="absolute z-0 object-cover w-auto h-auto mb-2 transform -translate-y-1/2 -scale-x-100 -scale-y-100 -left-28 top-1/2">
+                
+                <div class="relative flex flex-col items-center p-8 bg-white rounded-lg shadow-2xl ">
+                    <div class="relative flex flex-col items-center w-24 h-24 p-4 bg-green-500 rounded-full shadow-2xl md:p-8">
+                    <img src="{{ asset('images/Phone Message.png') }}" alt="Image 1" class="relative object-cover w-24 h-24 mb-2 ">
+                    </div>
+                    <p class="relative font-semibold text-gray-800 ">Konsultasi</p>
                 </div>
             </div>
-
+    
             <!-- Item 2 -->
-            <div
-                class="relative flex flex-col items-center w-24 h-28 p-4 md:w-auto md:h-auto md:p-8 bg-white rounded-lg shadow-2xl">
-                <img src="{{ asset('images/mastercard.png') }}" alt="Image 2"
-                    class="relative object-cover w-8 h-8 md:w-24 md:h-24 mb-2">
-                <p class="relative text-sm md:text-lg text-gray-800 text-center">Kalkulator Zakat</p>
+            <div class="relative flex flex-col items-center w-24 p-4 bg-white rounded-lg shadow-2xl h-28 md:w-auto md:h-auto md:p-8">
+                <div class="relative flex flex-col items-center w-24 h-24 p-4 bg-green-500 rounded-full shadow-2xl md:p-8">
+                <img src="{{ asset('images/Calculator.png') }}" alt="Image 2" class="relative object-cover w-8 h-8 mb-2 md:w-24 md:h-24">
+                </div>
+                <p class="relative text-sm text-center text-gray-800 md:text-lg">Kalkulator Zakat</p>
             </div>
-
+    
             <!-- Item 3 -->
-            <div
-                class="relative flex flex-col items-center w-24 h-28 p-4 md:w-auto md:h-auto md:p-8 bg-white rounded-lg shadow-2xl">
-                <img src="{{ asset('images/mastercard.png') }}" alt="Image 3"
-                    class="relative object-cover w-8 h-8 md:w-24 md:h-24 mb-2">
-                <p class="relative text-sm md:text-lg text-gray-800 text-center">Rekenign Donasi</p>
+            <div class="relative flex flex-col items-center p-8 bg-white rounded-lg shadow-2xl ">
+                <div class="relative flex flex-col items-center w-24 h-24 p-4 bg-green-500 rounded-full shadow-2xl md:p-8">
+                <img src="{{ asset('images/Buy With Card.png') }}" alt="Image 3" class="relative object-cover w-24 h-24 mb-2 ">
+                </div>
+                <p class="relative font-semibold text-gray-800 ">Rekenign Donasi</p>
             </div>
-
+    
             <!-- Wrapper for Right Ornament and Item 4 -->
             <div class="relative flex items-center">
-                <img src="{{ asset('images/fknleaf.png') }}" alt="Right Ornament"
-                    class="absolute z-0 object-cover w-16 h-auto md:w-auto md:h-auto mb-2 transform -translate-y-1/2 -right-8 md:-right-28 top-1/2">
-
-                <div
-                    class="relative flex flex-col items-center w-24 h-28 p-4 md:w-auto md:h-auto md:p-8 bg-white rounded-lg shadow-2xl">
-                    <img src="{{ asset('images/qr.png') }}" alt="Image 4"
-                        class="relative object-cover w-8 h-8 md:w-24 md:h-24 mb-2">
-                    <p class="relative text-sm md:text-lg text-gray-800 text-center">QR Donasi</p>
+                <img src="{{ asset('images/fknleaf.png') }}" alt="Right Ornament" class="absolute z-0 object-cover w-auto h-auto mb-2 transform -translate-y-1/2 -right-28 top-1/2">
+                
+                <div class="relative flex flex-col items-center p-8 bg-white rounded-lg shadow-2xl ">
+                    <div class="relative flex flex-col items-center w-24 h-24 p-4 bg-green-500 rounded-full shadow-2xl md:p-8">
+                    <img src="{{ asset('images/Qr Code.png') }}" alt="Image 4" class="relative object-cover w-24 h-24 mb-2 ">
+                    </div>  
+                    <p class="relative font-semibold text-gray-800 ">QR Donasi</p>
                 </div>
             </div>
         </div>
     </div>
+    
+    
+    
+
+    {{-- <div class="relative z-10">
+        <div class="absolute top-0 left-0 right-0 grid grid-cols-1 gap-4 py-10 mx-20 -mt-32 bg-white shadow-lg md:grid-cols-2 lg:grid-cols-4 rounded-3xl"
+            >
+            <!-- Item -->
+            <div class="flex flex-col items-center">
+                <img src="{{ asset('images/penerima_manfaat.png') }}" alt="Image 1"
+                    class="object-cover w-24 h-24 mb-2">
+                <p class="mb-1 font-semibold text-green-500">5000</p>
+                <p class="text-gray-700">Penerima Manfaat</p>
+            </div>
+            <!-- Item -->
+            <div class="flex flex-col items-center">
+                <img src="{{ asset('images/penghimpunan.png') }}" alt="Image 2" class="object-cover w-24 h-24 mb-2">
+                <p class="mb-1 font-semibold text-green-500">Rp 2.000.000.000.000</p>
+                <p class="text-gray-700">Penghimpunan</p>
+            </div>
+            <!-- Item -->
+            <div class="flex flex-col items-center">
+                <img src="{{ asset('images/penyaluran.png') }}" alt="Image 3" class="object-cover w-24 h-24 mb-2">
+                <p class="mb-1 font-semibold text-green-500">Rp 2.000.000.000.000</p>
+                <p class="text-gray-700">Penyaluran</p>
+            </div>
+            <!-- Item -->
+            <div class="flex flex-col items-center">
+                <img src="{{ asset('images/donatur.png') }}" alt="Image 4" class="object-cover w-24 h-24 mb-2">
+                <p class="mb-1 font-semibold text-green-500">5000</p>
+                <p class="text-gray-700">Donatur</p>
+            </div>
+        </div>
+    </div> --}}
 
 
 
+    
 
+    {{-- zakat --}}
+    {{-- <div class="flex flex-col items-center w-full py-10 mt-4 bg-white bg-center bg-cover shadow-md"
+        style="background-image: url('{{ asset('images/zakat_bg.png') }}');">
+        <!-- Title -->
+        <div class="mb-8">
+            <h2 class="text-3xl font-bold text-green-500">Apakah Anda masih bingung dengan cara berzakat?</h2>
+        </div>
+
+        <!-- Container for items -->
+        <div class="relative flex items-start justify-around w-full max-w-4xl mx-20 space-x-20"
+            style="margin-top: -20px;">
+
+            <!-- Overlay container for content -->
+            <div class="flex items-start justify-center w-full mx-14">
+                <!-- Item 1 -->
+                <div class="items-center p-4 rounded-lg ">
+                    <a href="" class="px-4 py-2 text-white bg-green-500 rounded-lg"> Konsultasi Zakat</a>
+                </div>
+
+                <!-- Item 2 -->
+                <div class="items-center p-4 rounded-lg ">
+                    <a href="" class="px-4 py-2 text-white bg-green-500 rounded-lg"> Kalkulator Zakat</a>
+                </div>
+
+                <!-- Item 3 -->
+                <div class="items-center p-4 rounded-lg ">
+                    <a href="" class="px-4 py-2 text-white bg-green-500 rounded-lg"> Konfirmasii Zakat</a>
+                </div>
+            </div>
+        </div>
+    </div> --}}
 
     {{-- campaign --}}
     <div class="flex flex-col items-center py-10 bg-white">
         <!-- Title -->
-        <div class="mb-8 w-full flex items-center justify-between">
-            <div class="relative px-12 flex flex-col justify-between">
-                <h2 class="text-lg font-semibold text-green-600">Campaign Lazisnu Cilacap</h2>
-                <h2 class="text-sm text-black">Berikut merupakan campaign terbaru Lazisnu Cilacap</h2>
+        <div class="mb-8">
+            <h2 class="text-xl font-semibold text-black">Mari Mulai Berdonasi</h2>
+            <div class="relative px-8 pt-4 mt-2">
+                <div class="absolute top-0 left-0 right-0 h-1 bg-gradient-to-r from-white via-green-700 to-white">
+                </div>
             </div>
-            <button class="relative px-4 mr-12 py-2 text-white rounded-md bg-green-500 hover:bg-green-600 ">
-                Campaign Lainya
-            </button>
         </div>
         <div class="grid grid-cols-1 gap-6 mx-10 sm:grid-cols-2 lg:grid-cols-3">
             <!-- Card 1 -->
@@ -85,20 +146,22 @@
             <x-campaign-card />
             <x-campaign-card />
             <x-campaign-card />
+        </div>
+        <div class="mt-12">
+            <button class="px-4 py-2 text-white bg-green-500 rounded-md ">Campaign Lainnya</button>
         </div>
     </div>
 
     {{-- berita --}}
-    <div class="flex flex-col items-center w-full py-10 mt-4 bg-gray-50 bg-opacity-90 bg-center bg-cover shadow-md">
+    <div class="flex flex-col items-center w-full py-10 mt-4 bg-white bg-center bg-cover shadow-md"
+        style="background-image: url('{{ asset('images/berita_bg.png') }}');">
         <!-- Title -->
-        <div class="mb-8 w-full flex items-center justify-between">
-            <div class="relative px-12 flex flex-col justify-between">
-                <h2 class="text-lg font-semibold text-green-600">Berita Lazisnu Cilacap</h2>
-                <h2 class="text-sm text-black">Berikut merupakan berita terbaru Lazisnu Cilacap</h2>
+        <div class="mb-8">
+            <h2 class="text-xl font-semibold text-black">Berita</h2>
+            <div class="relative px-8 pt-4 mt-2">
+                <div class="absolute top-0 left-0 right-0 h-1 bg-gradient-to-r from-white via-green-700 to-white">
+                </div>
             </div>
-            <button class="relative px-4 mr-12 py-2 text-white rounded-md bg-green-500 hover:bg-green-600 ">
-                Berita Lainya
-            </button>
         </div>
 
         <div class="grid grid-cols-1 gap-6 mx-10 sm:grid-cols-2 lg:grid-cols-3">
@@ -107,6 +170,9 @@
             <x-berita-card />
             <x-berita-card />
 
+        </div>
+        <div class="mt-12">
+            <button class="px-4 py-2 text-white bg-green-500 rounded-md ">Berita Lainnya</button>
         </div>
     </div>
     {{-- tentang --}}
@@ -120,47 +186,29 @@
             </div>
         </div>
 
-        <!-- Container for items left -->
+        <!-- Container for items -->
         <div class="flex items-start justify-between w-full max-w-4xl mx-20 space-x-20">
             <!-- Item 1 -->
-            <div class="flex flex-col items-center flex-1">
+            {{-- <div class="flex flex-col items-center flex-1">
                 <img src="{{ asset('images/tentang.png') }}" alt="Image 1" class="object-cover mb-4 w-128 h-128">
-            </div>
+            </div> --}}
 
             <!-- Item 2 -->
             <div class="flex flex-col items-start flex-1 text-justify">
-                <div>
-                    <p class="mb-4 text-xl font-extrabold text-green-600">Sekilas NU Care-LAZISNU Cilacap</p>
-                    <p class="mb-4 text-md text-gray-700">
-                        Terima kasih banyak kami ucapkan kepada para muzakki, munfiq, donatur atas semua zakat, infaq,
-                        sedekah atas donasinya yang telah diamanatkan melalui NU Care Lazisnu Cilacap.
-                    </p>
-                </div>
-        <!-- Container for items left -->
-                <div class="w-full flex flex-col justify-center">
-                    <p class="mb-4 text-lg font-semibold text-black text-center">
-                        Terima kasih kepada Sejumlah
-                    </p>
-                    <div class="flex flex-row justify-center ">
-                    <div
-                        class="relative flex flex-col items-center w-24 h-24 p-4 md:w-60 md:h-auto md:p-8">
-                        <img src="{{ asset('images/talk.png') }}" alt="Image 1"
-                            class="relative w-8 h-8 md:w-16 md:h-16 mb-2">
-                        <p class="relative text-sm md:text-md font-semibold text-green-600 text-center">41.124 muzakki
-                            <br>
-                            NU Care Lazisnu Cilacap</p>
-                    </div>
-                    <div
-                        class="relative flex flex-col items-center w-24 h-24 p-4 md:w-60 md:h-auto md:p-8">
-                        <img src="{{ asset('images/talk.png') }}" alt="Image 1"
-                            class="relative w-8 h-8 md:w-16 md:h-16 mb-2">
-                        <p class="relative text-sm md:text-md font-semibold text-green-600 text-center">64.712 Munfiq
-                            <br>
-                            NU Care Lazisnu Cilacap</p>
-                    </div>
-                </div>
-
-                </div>
+                <p class="mb-4 text-xl font-extrabold text-black">Sekilas NU Care-LAZISNU Cilacap</p>
+                <p class="mb-4 text-sm text-gray-700">
+                    NU Care-LAZISNU adalah rebranding dan/atau sebagai pintu masuk agar masyarakat global mengenal
+                    Lembaga Amil Zakat, Infak, dan Sedekah Nahdlatul Ulama (LAZISNU) sebagai lembaga filantropi NU. NU
+                    Care-LAZISNU berdiri pada tahun 2004 sebagai sarana untuk membantu masyarakat, sesuai amanat
+                    muktamar NU yang ke-31 di Asrama Haji Donohudan, Boyolali, Jawa Tengah. LAZISNU secara
+                    yuridis-formal dikukuhkan oleh SK Menteri Agama RI No. 65/2005 untuk melakukan penghimpunan Zakat,
+                    Infak, dan Sedekah (ZIS) kepada masyarakat luas.
+                    <br><br>
+                    NU Care-LAZISNU merupakan lembaga nirlaba milik perkumpulan Nahdlatul Ulama (NU) yang bertujuan
+                    untuk berkhidmat dalam rangka membantu kesejahteraan dan kemandirian umat; mengangkat harkat sosial
+                    dengan mendayagunakan dana Zakat, Infak, Sedekah (ZIS) dan dana sosial-keagamaan lainnya (DSKL).
+                </p>
+                <button class="px-4 py-2 text-white bg-green-500 rounded-lg ">Baca Selengkap</button>
             </div>
         </div>
     </div>
@@ -168,11 +216,11 @@
     <div class="flex flex-col items-center w-full py-10 mt-4 bg-white bg-center bg-cover shadow-md">
         <!-- Title -->
         <div class="mb-4">
-            <h2 class="text-xl font-semibold text-green-600">Mitra Kami</h2>
-            {{-- <div class="relative px-8 pt-4 mt-2">
+            <h2 class="text-xl font-semibold text-black">Mitra Kami</h2>
+            <div class="relative px-8 pt-4 mt-2">
                 <div class="absolute top-0 left-0 right-0 h-1 bg-gradient-to-r from-white via-green-700 to-white">
                 </div>
-            </div> --}}
+            </div>
         </div>
         <div class="flex flex-wrap justify-center block inline mt-4 space-x-8">
             <x-mitra-logo />
