ecaeac5c3a6e2832259ca1328cca51bac8630967
Mas Dzuky Ar-Rouf <127199885+Einkelberg@users.noreply.github.com>
2024-08-05 13:36:54 +0700

    Update landing.blade.php

--- resources/views/livewire/landing.blade.php
+++ resources/views/livewire/landing.blade.php
@@ -14,55 +14,68 @@
     </div>
 
     <div class="relative z-12 w-screen flex flex-wrap items-center justify-center">
-        <div class="relative flex flex-wrap items-center justify-center h-auto py-10 md:mx-20 -mt-24 md:-mt-32 space-x-2 md:space-x-16 rounded-3xl">
-            
+        <div
+            class="relative flex flex-wrap items-center justify-center h-auto py-10 md:mx-20 -mt-24 md:-mt-32 space-x-2 md:space-x-16 rounded-3xl">
+
             <!-- Wrapper for Left Ornament and Item 1 -->
             <div class="relative flex items-center">
-                <img src="{{ asset('images/fknleaf.png') }}" alt="Left Ornament" class="absolute z-0 object-cover w-16 h-auto md:w-auto md:h-auto mb-2 transform -translate-y-1/2 -scale-x-100 -scale-y-100 -left-8 md:-left-28 top-1/2">
-                
-                <div class="relative flex flex-col items-center w-24 h-28 p-4 md:w-auto md:h-auto md:p-8 bg-white rounded-lg shadow-2xl">
-                    <img src="{{ asset('images/talk.png') }}" alt="Image 1" class="relative object-cover w-8 h-8 md:w-24 md:h-24 mb-2">
+                <img src="{{ asset('images/fknleaf.png') }}" alt="Left Ornament"
+                    class="absolute z-0 object-cover w-16 h-auto md:w-auto md:h-auto mb-2 transform -translate-y-1/2 -scale-x-100 -scale-y-100 -left-8 md:-left-28 top-1/2">
+
+                <div
+                    class="relative flex flex-col items-center w-24 h-28 p-4 md:w-auto md:h-auto md:p-8 bg-white rounded-lg shadow-2xl">
+                    <img src="{{ asset('images/talk.png') }}" alt="Image 1"
+                        class="relative object-cover w-8 h-8 md:w-24 md:h-24 mb-2">
                     <p class="relative text-sm md:text-lg text-gray-800 text-center">Konsultasi</p>
                 </div>
             </div>
-    
+
             <!-- Item 2 -->
-            <div class="relative flex flex-col items-center w-24 h-28 p-4 md:w-auto md:h-auto md:p-8 bg-white rounded-lg shadow-2xl">
-                <img src="{{ asset('images/mastercard.png') }}" alt="Image 2" class="relative object-cover w-8 h-8 md:w-24 md:h-24 mb-2">
+            <div
+                class="relative flex flex-col items-center w-24 h-28 p-4 md:w-auto md:h-auto md:p-8 bg-white rounded-lg shadow-2xl">
+                <img src="{{ asset('images/mastercard.png') }}" alt="Image 2"
+                    class="relative object-cover w-8 h-8 md:w-24 md:h-24 mb-2">
                 <p class="relative text-sm md:text-lg text-gray-800 text-center">Kalkulator Zakat</p>
             </div>
-    
+
             <!-- Item 3 -->
-            <div class="relative flex flex-col items-center w-24 h-28 p-4 md:w-auto md:h-auto md:p-8 bg-white rounded-lg shadow-2xl">
-                <img src="{{ asset('images/mastercard.png') }}" alt="Image 3" class="relative object-cover w-8 h-8 md:w-24 md:h-24 mb-2">
+            <div
+                class="relative flex flex-col items-center w-24 h-28 p-4 md:w-auto md:h-auto md:p-8 bg-white rounded-lg shadow-2xl">
+                <img src="{{ asset('images/mastercard.png') }}" alt="Image 3"
+                    class="relative object-cover w-8 h-8 md:w-24 md:h-24 mb-2">
                 <p class="relative text-sm md:text-lg text-gray-800 text-center">Rekenign Donasi</p>
             </div>
-    
+
             <!-- Wrapper for Right Ornament and Item 4 -->
             <div class="relative flex items-center">
-                <img src="{{ asset('images/fknleaf.png') }}" alt="Right Ornament" class="absolute z-0 object-cover w-16 h-auto md:w-auto md:h-auto mb-2 transform -translate-y-1/2 -right-8 md:-right-28 top-1/2">
-                
-                <div class="relative flex flex-col items-center w-24 h-28 p-4 md:w-auto md:h-auto md:p-8 bg-white rounded-lg shadow-2xl">
-                    <img src="{{ asset('images/qr.png') }}" alt="Image 4" class="relative object-cover w-8 h-8 md:w-24 md:h-24 mb-2">
+                <img src="{{ asset('images/fknleaf.png') }}" alt="Right Ornament"
+                    class="absolute z-0 object-cover w-16 h-auto md:w-auto md:h-auto mb-2 transform -translate-y-1/2 -right-8 md:-right-28 top-1/2">
+
+                <div
+                    class="relative flex flex-col items-center w-24 h-28 p-4 md:w-auto md:h-auto md:p-8 bg-white rounded-lg shadow-2xl">
+                    <img src="{{ asset('images/qr.png') }}" alt="Image 4"
+                        class="relative object-cover w-8 h-8 md:w-24 md:h-24 mb-2">
                     <p class="relative text-sm md:text-lg text-gray-800 text-center">QR Donasi</p>
                 </div>
             </div>
         </div>
     </div>
-    
-    
-    
+
+
+
 
 
     {{-- campaign --}}
     <div class="flex flex-col items-center py-10 bg-white">
         <!-- Title -->
-        <div class="mb-8">
-            <h2 class="text-xl font-semibold text-black">Mari Mulai Berdonasi</h2>
-            <div class="relative px-8 pt-4 mt-2">
-                <div class="absolute top-0 left-0 right-0 h-1 bg-gradient-to-r from-white via-green-700 to-white">
-                </div>
+        <div class="mb-8 w-full flex items-center justify-between">
+            <div class="relative px-12 flex flex-col justify-between">
+                <h2 class="text-lg font-semibold text-green-600">Campaign Lazisnu Cilacap</h2>
+                <h2 class="text-sm text-black">Berikut merupakan campaign terbaru Lazisnu Cilacap</h2>
             </div>
+            <button class="relative px-4 mr-12 py-2 text-white rounded-md bg-green-500 hover:bg-green-600 ">
+                Campaign Lainya
+            </button>
         </div>
         <div class="grid grid-cols-1 gap-6 mx-10 sm:grid-cols-2 lg:grid-cols-3">
             <!-- Card 1 -->
@@ -72,22 +85,20 @@
             <x-campaign-card />
             <x-campaign-card />
             <x-campaign-card />
-        </div>
-        <div class="mt-12">
-            <button class="px-4 py-2 text-white bg-green-500 rounded-md ">Campaign Lainnya</button>
         </div>
     </div>
 
     {{-- berita --}}
-    <div class="flex flex-col items-center w-full py-10 mt-4 bg-white bg-center bg-cover shadow-md"
-        style="background-image: url('{{ asset('images/berita_bg.png') }}');">
+    <div class="flex flex-col items-center w-full py-10 mt-4 bg-gray-50 bg-opacity-90 bg-center bg-cover shadow-md">
         <!-- Title -->
-        <div class="mb-8">
-            <h2 class="text-xl font-semibold text-black">Berita</h2>
-            <div class="relative px-8 pt-4 mt-2">
-                <div class="absolute top-0 left-0 right-0 h-1 bg-gradient-to-r from-white via-green-700 to-white">
-                </div>
+        <div class="mb-8 w-full flex items-center justify-between">
+            <div class="relative px-12 flex flex-col justify-between">
+                <h2 class="text-lg font-semibold text-green-600">Berita Lazisnu Cilacap</h2>
+                <h2 class="text-sm text-black">Berikut merupakan berita terbaru Lazisnu Cilacap</h2>
             </div>
+            <button class="relative px-4 mr-12 py-2 text-white rounded-md bg-green-500 hover:bg-green-600 ">
+                Berita Lainya
+            </button>
         </div>
 
         <div class="grid grid-cols-1 gap-6 mx-10 sm:grid-cols-2 lg:grid-cols-3">
@@ -96,9 +107,6 @@
             <x-berita-card />
             <x-berita-card />
 
-        </div>
-        <div class="mt-12">
-            <button class="px-4 py-2 text-white bg-green-500 rounded-md ">Berita Lainnya</button>
         </div>
     </div>
     {{-- tentang --}}
@@ -112,29 +120,47 @@
             </div>
         </div>
 
-        <!-- Container for items -->
+        <!-- Container for items left -->
         <div class="flex items-start justify-between w-full max-w-4xl mx-20 space-x-20">
             <!-- Item 1 -->
-            {{-- <div class="flex flex-col items-center flex-1">
+            <div class="flex flex-col items-center flex-1">
                 <img src="{{ asset('images/tentang.png') }}" alt="Image 1" class="object-cover mb-4 w-128 h-128">
-            </div> --}}
+            </div>
 
             <!-- Item 2 -->
             <div class="flex flex-col items-start flex-1 text-justify">
-                <p class="mb-4 text-xl font-extrabold text-black">Sekilas NU Care-LAZISNU Cilacap</p>
-                <p class="mb-4 text-sm text-gray-700">
-                    NU Care-LAZISNU adalah rebranding dan/atau sebagai pintu masuk agar masyarakat global mengenal
-                    Lembaga Amil Zakat, Infak, dan Sedekah Nahdlatul Ulama (LAZISNU) sebagai lembaga filantropi NU. NU
-                    Care-LAZISNU berdiri pada tahun 2004 sebagai sarana untuk membantu masyarakat, sesuai amanat
-                    muktamar NU yang ke-31 di Asrama Haji Donohudan, Boyolali, Jawa Tengah. LAZISNU secara
-                    yuridis-formal dikukuhkan oleh SK Menteri Agama RI No. 65/2005 untuk melakukan penghimpunan Zakat,
-                    Infak, dan Sedekah (ZIS) kepada masyarakat luas.
-                    <br><br>
-                    NU Care-LAZISNU merupakan lembaga nirlaba milik perkumpulan Nahdlatul Ulama (NU) yang bertujuan
-                    untuk berkhidmat dalam rangka membantu kesejahteraan dan kemandirian umat; mengangkat harkat sosial
-                    dengan mendayagunakan dana Zakat, Infak, Sedekah (ZIS) dan dana sosial-keagamaan lainnya (DSKL).
-                </p>
-                <button class="px-4 py-2 text-white bg-green-500 rounded-lg ">Baca Selengkap</button>
+                <div>
+                    <p class="mb-4 text-xl font-extrabold text-green-600">Sekilas NU Care-LAZISNU Cilacap</p>
+                    <p class="mb-4 text-md text-gray-700">
+                        Terima kasih banyak kami ucapkan kepada para muzakki, munfiq, donatur atas semua zakat, infaq,
+                        sedekah atas donasinya yang telah diamanatkan melalui NU Care Lazisnu Cilacap.
+                    </p>
+                </div>
+        <!-- Container for items left -->
+                <div class="w-full flex flex-col justify-center">
+                    <p class="mb-4 text-lg font-semibold text-black text-center">
+                        Terima kasih kepada Sejumlah
+                    </p>
+                    <div class="flex flex-row justify-center ">
+                    <div
+                        class="relative flex flex-col items-center w-24 h-24 p-4 md:w-64 md:h-auto md:p-8">
+                        <img src="{{ asset('images/talk.png') }}" alt="Image 1"
+                            class="relative w-8 h-8 md:w-16 md:h-16 mb-2">
+                        <p class="relative text-sm md:text-md font-semibold text-green-600 text-center">41.124 muzakki
+                            <br>
+                            NU Care Lazisnu Cilacap</p>
+                    </div>
+                    <div
+                        class="relative flex flex-col items-center w-24 h-24 p-4 md:w-64 md:h-auto md:p-8">
+                        <img src="{{ asset('images/talk.png') }}" alt="Image 1"
+                            class="relative w-8 h-8 md:w-16 md:h-16 mb-2">
+                        <p class="relative text-sm md:text-md font-semibold text-green-600 text-center">64.712 Munfiq
+                            <br>
+                            NU Care Lazisnu Cilacap</p>
+                    </div>
+                </div>
+
+                </div>
             </div>
         </div>
     </div>
@@ -142,11 +168,11 @@
     <div class="flex flex-col items-center w-full py-10 mt-4 bg-white bg-center bg-cover shadow-md">
         <!-- Title -->
         <div class="mb-4">
-            <h2 class="text-xl font-semibold text-black">Mitra Kami</h2>
-            <div class="relative px-8 pt-4 mt-2">
+            <h2 class="text-xl font-semibold text-green-600">Mitra Kami</h2>
+            {{-- <div class="relative px-8 pt-4 mt-2">
                 <div class="absolute top-0 left-0 right-0 h-1 bg-gradient-to-r from-white via-green-700 to-white">
                 </div>
-            </div>
+            </div> --}}
         </div>
         <div class="flex flex-wrap justify-center block inline mt-4 space-x-8">
             <x-mitra-logo />
