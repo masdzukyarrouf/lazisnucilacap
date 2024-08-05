2324ecd7d5e91e488a669a7dee0446df841cacc0
Mas Dzuky Ar-Rouf <127199885+Einkelberg@users.noreply.github.com>
2024-08-05 13:45:47 +0700

    Update campaign-card.blade.php

--- resources/views/components/campaign-card.blade.php
+++ resources/views/components/campaign-card.blade.php
@@ -1,5 +1,10 @@
 <div class="overflow-hidden bg-white rounded-lg shadow-lg">
-    <img src="{{ asset('images/campaign_1.png') }}" alt="Picture" class="object-cover w-full">
+    <div class="relative group">
+        <img src="{{ asset('images/campaign_1.png') }}" alt="Picture" class="object-cover w-full hover:bg-green-600 hover:cursor-pointer">
+        <div class="absolute inset-0 flex items-center justify-center bg-green-600 bg-opacity-80 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+            <p class="text-white text-lg">View Campaign</p>
+        </div>
+    </div>
     <div class="p-4">
         <h2 class="text-xl font-semibold text-gray-800">Title 1</h2>
         <div class="flex items-center mt-1">
