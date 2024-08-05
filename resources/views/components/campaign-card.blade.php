1c64e29eb25f6cee9a5cef14396bac888dd4d995
Mas Dzuky Ar-Rouf <127199885+Einkelberg@users.noreply.github.com>
2024-08-05 16:40:56 +0700

    Update campaign-card.blade.php

--- resources/views/components/campaign-card.blade.php
+++ resources/views/components/campaign-card.blade.php
@@ -1,7 +1,7 @@
 <div class="flex flex-grow md:flex-col bg-white rounded-lg shadow-lg ">
     <div class="relative group flex items-center w-36 md:h-auto md:w-auto">
         <img src="{{ asset('images/campaign_1.png') }}" alt="Picture"
-            class="object-cover hover:bg-green-600 hover:cursor-pointer">
+            class="object-cover h-full w-full hover:cursor-pointer">
         <div
             class="hover:cursor-pointer rounded-lg absolute inset-0 flex items-center justify-center bg-green-600 bg-opacity-80 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
             <p class="text-white text-lg">View Campaign</p>
