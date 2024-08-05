103bbef0832d8d1a39173147b4f26d1fc2fbf413
Mas Dzuky Ar-Rouf <127199885+Einkelberg@users.noreply.github.com>
2024-08-05 16:41:00 +0700

    Update berita-card.blade.php

--- resources/views/components/berita-card.blade.php
+++ resources/views/components/berita-card.blade.php
@@ -1,19 +1,17 @@
-<div class="overflow-hidden bg-white shadow-lg">
-    <div class="relative group">
-        <img src="{{ asset('images/berita_1.png') }}" alt="Picture" class="object-cover w-full">
-        {{-- overlay --}}
-        {{-- <div class="absolute inset-0 flex items-center justify-center bg-green-600 bg-opacity-80 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-            <p class="text-white text-lg">View Berita</p>
-        </div> --}}
+<div class="flex flex-grow md:flex-col bg-white rounded-lg shadow-lg">
+    <div class="relative group flex items-center w-80 md:h-auto md:w-auto">
+        <img src="{{ asset('images/berita_1.png') }}" alt="Picture" class="object-cover w-full h-full">
     </div>
-    <div class="p-4">
-        <h2 class="text-sm font-semibold text-gray-800">Program Ramadhan 2024, Nu Care-Lazisnu Cilacap Berhasil
-            Menyalurkan Kepada 54.000 Penerima Manfaat</h2>
-        <div class="flex items-center mt-1 mb-4">
+    <div class="py-2 px-3 md:p-4">
+        <h2 class="text-sm md:text-xl font-semibold text-gray-800">
+            Program Ramadhan 2024, Nu Care-Lazisnu Cilacap Berhasil Menyalurkan Kepada 54.000 Penerima Manfaat
+        </h2>
+        <div class="flex items-center mt-1">
             <img src="{{ asset('images/clock.png') }}" alt="pinpoint" class="w-3 h-3">
-            <p class="pl-1 text-sm text-gray-600">Tanggal ea</p>
+            <p class="pl-1 text-xs md:text-sm text-gray-600">Tanggal ea</p>
         </div>
-
-        <a href="#" class="text-sm text-left text-green-500 hover:text-green-600 hover:cursor-pointer">Baca Selengkapnya  ...</a>
+        <a href="#" class="text-sm md:text-base text-green-500 hover:text-green-600 hover:cursor-pointer">
+            Baca Selengkapnya ...
+        </a>
     </div>
 </div>
