9efd2744936b1c995c175ea4b249d84d056586db
Mas Dzuky Ar-Rouf <127199885+Einkelberg@users.noreply.github.com>
2024-08-07 14:11:41 +0700

    new layout for admin

---
+++ resources/views/layouts/none.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    {{-- kirim title lewat class, default app name di env --}}
+    <title>{{ str_replace('_', ' ', $title ?? config('app.name')) }}</title>
+    @vite('resources/css/app.css')
+    <style>
+        body {
+            font-family: 'Inter', sans-serif;
+            margin: 0;
+        }
+    </style>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap" rel="stylesheet">
+    @livewireStyles
+</head>
+
+<body class="flex flex-col h-screen bg-gray-200 bg-opacity-80">
+    {{ $slot }}
+    @livewireScripts
+</body>
+
+</html>
