ec10eb959d9e4c48d14f7c5d2f0fe9672ff02748
Mas Dzuky Ar-Rouf <127199885+Einkelberg@users.noreply.github.com>
2024-08-07 15:07:49 +0700

    seeding purposes

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,11 +24,12 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
-            'remember_token' => Str::random(10),
+            'username' => fake()->name(),
+            'first_name' => fake()->name(),
+            'last_name' => fake()->name(),
+            'no_telp' => fake()->phoneNumber(),
+            'password' => static::$password ??= Hash::make('123'),
+            'role' => 'donatur',
         ];
     }
 
