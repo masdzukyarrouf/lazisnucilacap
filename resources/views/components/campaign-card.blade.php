b936e8743a40d4cf354d2ad94882aa17c4b1e4db
Mas Dzuky Ar-Rouf <127199885+Einkelberg@users.noreply.github.com>
2024-08-07 10:05:02 +0700

    Update campaign-card.blade.php

--- resources/views/components/campaign-card.blade.php
+++ resources/views/components/campaign-card.blade.php
@@ -1,4 +1,4 @@
-<div class="z-10 flex flex-grow md:flex-col bg-white rounded-lg shadow-lg outline-4 outline-gray-700 ">
+<div class="z-5 flex flex-grow md:flex-col bg-white rounded-lg shadow-lg outline-4 outline-gray-700 ">
     <div class="z-0 relative group flex items-center w-36 md:h-auto md:w-auto">
         <img src="{{ asset('images/campaign_1.png') }}" alt="Picture"
             class="object-cover h-full w-full hover:cursor-pointer">
