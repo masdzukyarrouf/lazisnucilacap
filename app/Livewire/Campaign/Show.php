d999a5f61cd9baea5b7a79cb8cafd511ddba76f0
Mas Dzuky Ar-Rouf <127199885+Einkelberg@users.noreply.github.com>
2024-08-13 15:14:53 +0700

    show detail donasi

--- app/Livewire/Campaign/Show.php
+++ app/Livewire/Campaign/Show.php
@@ -15,12 +15,25 @@ class Show extends Component
     {
         $this->id_campaign = $id_campaign;
         $this->campaign = Campaign::find($id_campaign);
+        // dd($this->campaign);
+
+        $this->processDescription();
     }
-    public function abc(){
-        dd('dsa');
+
+    public function processDescription()
+    {
+        $desc = $this->campaign->description;
+
+        $desc = str_replace('[img1]', '</p> <img src="' . asset('storage/images/campaign/' . $this->campaign->main_picture) . '" class="article-image" style="width: 200px; height: auto;" /> <p>', $desc);
+        $desc = str_replace('[img2]', '</p> <img src="' . asset('storage/images/campaign/' . $this->campaign->second_picture) . '" class="article-image" style="width: 200px; height: auto;" /> <p>', $desc);
+        $desc = str_replace('[img3]', '</p> <img src="' . asset('storage/images/campaign/' . $this->campaign->last_picture) . '" class="article-image" style="width: 200px; height: auto;" /> <p>', $desc);
+
+        $this->processedDesc = $desc;
     }
     public function render()
     {
-        return view('livewire.campaign.show');
+        return view('livewire.campaign.show',[
+            'processedDesc' => $this->processedDesc,
+        ]);
     }
 }
