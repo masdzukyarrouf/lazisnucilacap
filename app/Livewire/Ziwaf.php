0bb1079b5a804311fa86bb791b2a162ca07634ae
AndikaBastian <andikabastian20@gmail.com>
2024-08-15 15:57:57 +0700

    gawe ziwaf

---
+++ app/Livewire/Ziwaf.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class Ziwaf extends Component
+{
+    public function render()
+    {
+        return view('livewire.ziwaf');
+    }
+}
