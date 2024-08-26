220ad71b5fd7de49f7f558322b4cc9aacdbcd2df
Mas Dzuky Ar-Rouf <127199885+Einkelberg@users.noreply.github.com>
2024-08-26 14:37:56 +0700

    navbar sidebar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,30 +1,85 @@
-<nav class="sticky top-0 z-10 p-4 bg-white shadow-2xl md:hidden">
+<nav class="sticky top-0 z-30 p-4 bg-white shadow-2xl md:hidden">
     <div class="container flex items-center justify-between px-4 mx-auto">
         <!-- Logo -->
         <a href="/" class="flex-shrink-0">
             <img src="{{ asset('images/cooler_logo_lazisnu.png') }}" alt="Logo" class="w-auto h-8">
         </a>
 
-        <!-- Mobile Menu Button -->
-        <button id="menu-btn" class="text-gray-300 hover:text-black focus:outline-none">
-            <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16">
-                </path>
+        <!-- User Menu Button -->
+        <button id="user-menu-btn" class="relative flex items-center p-2  text-black rounded-full">
+            <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path>
             </svg>
         </button>
+        
     </div>
 </nav>
 
+<!-- User Menu Pop-up -->
+<div id="user-menu" class="absolute z-30 right-0 top-12 w-48 px-4 py-2 bg-white shadow-lg rounded-lg hidden">
+    <div class="flex flex-col">
+        <x-navlink title="Lazisnu Cilacap" :links="[
+            ['href' => '#', 'text' => 'Visi & Misi'],
+            ['href' => '#', 'text' => 'Profil & Jajaran Pengurus'],
+            ['href' => '#', 'text' => 'Sejarah'],
+            ['href' => '#', 'text' => 'Legalitas'],
+            ['href' => '#', 'text' => 'Standar Layanan (MANTAP)'],
+            ['href' => '#', 'text' => 'Laporan & Publikasi'],
+            ['href' => '#', 'text' => 'Mitra'],
+        ]" isDropdown="true" />
+        <x-navlink title="Pilar & Program" :links="[
+            ['href' => '#', 'text' => 'NU Care Berdaya (Ekonomi)'],
+            ['href' => '#', 'text' => 'NU Care Cerdas (Pendidikan)'],
+            ['href' => '#', 'text' => 'NU Care Sehat (Kesehatan)'],
+            ['href' => '#', 'text' => 'NU Care Damai (Dakwah & Kemanusiaan)'],
+            ['href' => '#', 'text' => 'NU Care Hijau (Lingkungan Hidup & Kebencanaan)'],
+        ]" isDropdown="true" />
+
+        <x-navlink title="Layanan" :links="[
+            ['href' => '#', 'text' => 'Konfirmasi Donasi'],
+            ['href' => '#', 'text' => 'Pengajuan Umum'],
+            ['href' => '#', 'text' => 'Layanan Mobisnu'],
+            ['href' => '#', 'text' => 'Gocap'],
+        ]" isDropdown="true" />
+
+        <x-navlink title="Ziswaf" url="/zakat"/>
+
+        <x-navlink title="Campaign" url="/campaigns" />
+        <x-navlink title="Berita" url="/berita" />
+
+        @if (Auth::check())
+            @if (Auth::user()->role == 'admin')
+                <a href="/admin"
+                    class="px-6 py-2 text-white bg-green-600 border rounded-xl hover:border-green-600 hover:bg-white hover:text-green-600">
+                    Admin
+                </a>
+            @elseif (Auth::user()->role == 'donatur')
+                <a href="/profil"
+                    class="px-6 py-2 text-white bg-green-600 border rounded-xl hover:border-green-600 hover:bg-white hover:text-green-600">
+                    profil
+                </a>
+            @endif
+        @else
+            <a href="/login"
+                class="px-6 py-2 z-30 text-white bg-green-600 border rounded-xl hover:border-green-600 hover:bg-white hover:text-green-600">
+                Login
+            </a>
+        @endif
+        <a href="/daftar"
+            class="px-6 py-2 text-green-600 border border-green-600 rounded-xl hover:bg-green-600 hover:text-white">Daftar</a>
+
+    </div>
+</div>
+
 <!-- Sidebar -->
 <div id="sidebar"
-    class="fixed inset-0 z-20 transition-transform transform -translate-x-full bg-white md:sticky md:top-0 md:w-full md:translate-x-0 md:flex md:items-center md:justify-between md:bg-transparent md:shadow-none md:p-0">
+    class="fixed inset-0 right-0 z-30 w-2/4 transition-transform transform -translate-x-full bg-white md:sticky md:top-0 md:w-full md:translate-x-0 md:flex md:items-center md:justify-between md:bg-transparent md:shadow-none md:p-0">
     <!-- Main Content -->
     <div
-        class="sticky top-0 flex flex-col items-center w-full px-8 py-4 space-y-4 bg-white md:flex-row md:justify-between md:space-y-0">
+        class="sticky right-0 top-0 flex flex-col items-center w-full px-8 py-4 space-y-4 bg-white md:flex-row md:justify-between md:space-y-0">
         <!-- Logo -->
         <a href="/" class="flex-shrink-0">
-            <img src="{{ asset('images/cooler_logo_lazisnu.png') }}" alt="Logo" class="w-auto h-8">
+            <img src="{{ asset('images/cooler_logo_lazisnu.png') }}" alt="Logo" class="hidden md:block w-auto h-8">
         </a>
 
         <!-- Navigation Links and Buttons -->
@@ -73,7 +128,7 @@
                 @endif
             @else
                 <a href="/login"
-                    class="px-6 py-2 text-white bg-green-600 border rounded-xl hover:border-green-600 hover:bg-white hover:text-green-600">
+                    class="px-6 py-2 z-30 text-white bg-green-600 border rounded-xl hover:border-green-600 hover:bg-white hover:text-green-600">
                     Login
                 </a>
             @endif
@@ -95,11 +150,28 @@
 </div>
 
 <script>
+    // Toggle User Menu visibility
+    document.getElementById('user-menu-btn').addEventListener('click', function() {
+        const userMenu = document.getElementById('user-menu');
+        userMenu.classList.toggle('hidden');
+    });
+
+    // Close User Menu when clicking outside
+    document.addEventListener('click', function(event) {
+        const userMenu = document.getElementById('user-menu');
+        const userMenuBtn = document.getElementById('user-menu-btn');
+        if (!userMenuBtn.contains(event.target) && !userMenu.contains(event.target)) {
+            userMenu.classList.add('hidden');
+        }
+    });
+
+    // Toggle Sidebar visibility
     document.getElementById('menu-btn').addEventListener('click', function() {
         const sidebar = document.getElementById('sidebar');
         sidebar.classList.toggle('-translate-x-full');
     });
 
+    // Close Sidebar
     document.getElementById('close-btn').addEventListener('click', function() {
         const sidebar = document.getElementById('sidebar');
         sidebar.classList.add('-translate-x-full');
--- resources/views/components/navlink.blade.php
+++ resources/views/components/navlink.blade.php
@@ -1,15 +1,15 @@
 <div class="relative flex items-center group">
     <a wire:navigate.hover href="{{ $url }}" 
-       class="inline-flex items-center  py-2 text-black rounded hover:text-green-500 {{ $class ? $class : '' }}">
+       class="inline-flex items-center py-2 text-black rounded hover:text-green-500 {{ $class ? $class : '' }}">
         {{ $title }}
         @if ($isDropdown)
             <img src="{{ asset('images/arrow_down.png') }}" alt="arrow down" class="w-4 h-4 ml-1">
         @endif
     </a>
     @if ($isDropdown && !empty($links))
-        <div class="absolute z-10 hidden text-xs md:text-lg text-black bg-gray-100 rounded shadow-lg group-hover:block top-full left-0 w-max">
+        <div class="absolute z-10 hidden text-xs md:text-lg text-black bg-gray-100 rounded shadow-lg group-hover:block top-full right-3/4 mr-14 md:mr-0 md:left-0 w-[240px]">
             @foreach ($links as $link)
-                <a href="{{ $link['href'] }}" class="block px-4 py-2 hover:text-green-500">{{ $link['text'] }}</a>
+                <a href="{{ $link['href'] }}" class=" break-words block px-4 py-2 hover:text-green-500 ">{{ $link['text'] }}</a>
             @endforeach
         </div>
     @endif
