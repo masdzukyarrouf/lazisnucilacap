bde0b1e027388afca6c65005333bc97b46a13ac4
AndikaBastian <andikabastian20@gmail.com>
2024-08-27 16:51:47 +0700

    done berita

--- resources/views/livewire/detail-berita.blade.php
+++ resources/views/livewire/detail-berita.blade.php
@@ -1,7 +1,7 @@
 
-    <div class="flex items-center justify-center px-6 lg:px-8">
-        <div class="bg-white rounded-lg shadow-md " style="width: 414px">
-            <x-nav-mobile2 title="Berita Lazisnu Cilacap" />
+    <div class="flex flex-col items-center justify-center">
+        <x-nav-mobile2 title="Berita Lazisnu Cilacap" />
+        <div class="min-h-screen bg-white shadow-md" style="width: 414px">
             <div class="mx-5 mb-10">
                 <img src="{{ asset('storage/' . $berita->picture) }}" alt="Main Picture" class="object-cover w-full h-full mt-4 rounded-md">
                 <div class="flex flex-col justify-between px-5">
@@ -19,7 +19,7 @@
                    <div class="flex flex-col gap-3">
                     @foreach ($otherBerita as $berita)
                         <a href="{{ route('detail-berita', ['id_berita' => $berita->id_berita]) }}">
-                            <div class="flex items-center p-2 bg-white rounded-lg shadow-md md:p-5 md:mx-2">
+                            <div class="flex items-center bg-white rounded-lg shadow-md">
                                 <img src="{{ asset('storage/' . $berita->picture) }}" alt="Main Picture" class="object-cover w-24 h-24 rounded-md">
                                 <div class="flex flex-col pl-2">
                                     <h2 class="text-sm font-semibold text-gray-800">
@@ -28,12 +28,15 @@
                                     <div class="flex items-center">
                                     <img src="{{ asset('images/clock.png') }}" alt="pinpoint" class="w-3 h-3">
                                     <p class="pl-1 text-xs text-gray-600 md:text-sm">{{ $berita->tanggal}}</p>
+                                    <h1 class="pl-4 text-xs text-green-500 md:text-sm">{{ $berita->kategori }}</h1>
                                     </div>
                                 </div>
                             </div>
                         </a>
                         @endforeach
-                </div> 
+                        <div class="mb-16">
+                        </div>
+                    </div> 
                 </div>
             </div>
         </div>
--- resources/views/livewire/user-berita/index.blade.php
+++ resources/views/livewire/user-berita/index.blade.php
@@ -34,15 +34,18 @@
                         <div class="flex flex-col gap-3">
                             @foreach ($otherBerita->skip(3) as $berita)
                             <a href="{{ route('detail-berita', ['id_berita' => $berita->id_berita]) }}">
-                                <div class="flex items-center p-2 bg-white rounded-lg shadow-md md:p-5 md:mx-2">
+                                <div class="flex items-center bg-white rounded-lg shadow-md">
                                     <img src="{{ asset('storage/' . $berita->picture) }}" alt="Main Picture" class="object-cover w-24 h-24 rounded-md">
                                     <div class="flex flex-col pl-2">
                                         <h2 class="text-sm font-semibold text-gray-800">
                                             {{ \Illuminate\Support\Str::limit($berita->title_berita, 30, '...') }}
                                         </h2>
-                                        <div class="flex items-center">
-                                            <img src="{{ asset('images/clock.png') }}" alt="pinpoint" class="w-3 h-3">
-                                            <p class="pl-1 text-xs text-gray-600 md:text-sm">{{ $berita->tanggal }}</p>
+                                        <div class="flex items-center justify-between mt-5">
+                                            <div class="flex items-center">
+                                                <img src="{{ asset('images/clock.png') }}" alt="pinpoint" class="w-3 h-3">
+                                                <h1 class="pl-1 text-xs text-gray-600 md:text-sm">{{ $berita->tanggal }}</h1>
+                                            </div>
+                                            <h1 class="pl-4 text-xs text-green-500 md:text-sm">{{ $berita->kategori }}</h1>
                                         </div>
                                     </div>
                                 </div>
