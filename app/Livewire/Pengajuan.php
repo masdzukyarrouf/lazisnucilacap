e9525d9f6e40ebd5cf785165ee5052c541429750
Mas Dzuky Ar-Rouf <127199885+Einkelberg@users.noreply.github.com>
2024-09-02 16:43:33 +0700

    pengajuan form

---
+++ app/Livewire/Pengajuan.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class Pengajuan extends Component
+{
+
+
+    public function render()
+    {
+        return view('livewire.pengajuan')->layout('layouts.mobile');
+    }
+}
