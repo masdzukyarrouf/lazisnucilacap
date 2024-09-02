f12b50e571bacb48e18d863a241050976b08fa67
Mas Dzuky Ar-Rouf <127199885+Einkelberg@users.noreply.github.com>
2024-09-02 10:49:06 +0700

    update on navmobile

--- resources/views/livewire/donasi/index.blade.php
+++ resources/views/livewire/donasi/index.blade.php
@@ -1,5 +1,5 @@
 <div class="flex flex-col items-center justify-center ">
-    <x-nav-mobile2 title="Nominal Donasi" />
+    <x-nav-mobile2 title="Nominal Donasi" :backUrl="route('campaign')"/>
     <div class=" w-full max-w-[414px] mx-auto bg-gray-100 py-4">
         <div class="w-full ">
             <!-- Kategori and Filter Buttons -->
