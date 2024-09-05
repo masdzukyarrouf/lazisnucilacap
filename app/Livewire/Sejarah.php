5674a3e6b1902b3acb1f6e0e3781e2ecb145d2ef
AndikaBastian <andikabastian20@gmail.com>
2024-09-05 14:14:04 +0700

    legalitas

---
+++ app/Livewire/Sejarah.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class Sejarah extends Component
+{
+    public function render()
+    {
+        return view('livewire.sejarah')->layout('layouts.mobile');
+    }
+}
---
+++ resources/views/livewire/sejarah.blade.php
@@ -0,0 +1,46 @@
+<div class="flex flex-col items-center justify-center">
+    <x-nav-mobile2 title="Sejarah" />
+    <div class="flex flex-col h-full min-h-screen bg-gray-100 shadow-md" style="width: 414px;">
+        <div class="flex flex-col gap-3">
+            <div class="p-6 bg-white">
+                <h1 class="pb-2 font-semibold text-green-500">
+                    2018
+                </h1>
+                <p class="text-sm">
+                    Setelah Konferensi Pengurus Cabang Nahdlatul Ulama Cilacap tahun 2018, diberikan amanah kepada H.Wasbah Samudra Fawaid, SE sebagai Ketua PC LAZISNU Cilacap. untuk memimpin LAZISNU Cilacap masa kepengurusan 2018-2023. Hal itu diperkuat oleh SK Pengurus Cabang Nahdlatul Ulama (PCNU) Cilacap No. 023/PC/A.II/11.34/VIII/18 tentang Susunan Pengurus Cabang LAZISNU Cilacap periode 2018 – 2023. NU Care Lazisnu Cilacap juga telah mendapatkan ijijn operasional dari LAZISNU PBNU berdasarkan SK LAZISNU PBNU nomor : 159/SK/PP-LAZISNU/IX/2018, nomor : 333/SK/PP-LAZISNU/X/2020 dan nomor : 062/SK/A.II/LAZISNU-PBNU/IX/2022 tentang pengesahan dan pemberian ijin operasional kepada NUCARE LAZISNU Cilacap
+                </p>
+            </div>
+            <div class="p-6 bg-white">
+                <h1 class="pb-2 font-semibold text-green-500">
+                    2018 - 2019
+                </h1>
+                <p class="text-sm">
+                    2018 - 2019 : pada tahun 2018-2019 NU Care Lazisnu Cilacap focus kepada penataan internal seputar tata kelola NU Care Lazisnu Cilacap, mulai dari penyusunan regulasi pengelolaan NU Care Lazisnu Cilacap,pengurusan ijin operasional NU Care Lazisnu Cilacap periode 2018-2020 training dan konsultasi Bersama konsultan hingga pembentukan Manajemen Eksekutif NU Care Lazisnu Cilacap dengan menunjuk Ahmad Fauzi,S.Pd.I sebagai direktur Manajemen Eksekutif NU Care Lazisnu Cilacap dan diberikan tugas untuk melakukan penataan manajemen ekskeutif NU Care Lazisnu Cilacap, NU Care Lazisnu Cilacap juga fokus terhadap penyusunan roadmap kinerja NU Care Lazisnu Cilacap dan pengembangan jaringan. Di tahun yang sama, NU Care Lazisnu Cilacap memperkuat kaderisasi amil secara nasional melalui kegiatan Madrasah Amil. Kala itu NU Care Lazisnu Cilacap adalah LAZISNU ke 4 LAZISNU di Indonesia yang menyelenggarakan Madrasah Amil
+                </p>
+                <p class="pt-2 text-sm">
+                    Selain itu, NU Care Lazisnu Cilacap juga terus memperkuat sinergi antar-lembaga dan Banom NU dalam gerakan tanggap bencana baik di wilayah kabupaten cilacap maupun di luar wilayah kabupaten cilacap dalam bendera NU Peduli, yang berfokus pada kegiatan kemanusiaan skala besar. Dan pada bulan September 2018 bertempat di Ponpes Nurul Islam Karangjati – Sampang, Melalui Komisi Fundraising Musyawarah Kerja Cabang ke 1 PCNU Cilacap periode 2018-2023 memutuskan Gerakan Koin NU Cilacap (GOCAP) diserahkan sepenuhnya kepada NU Care Lazisnu Cilacap. NU Care-LAZISNU Cilacap diberikan mandat untuk menyusun regulasi dan manajemen pengelolaan Gerakan Koin NU Cilacap (GOCAP). GOCAP menjadi sebagai langkah awal NU Care Lazisnu Cilacap dalam menjalankan program kerja pada masa awal periode dan sekaligus pengembangan program infaq yang sebelumnya bernama infaq sehari (seribu setiap hari).
+                </p>
+            </div>
+            <div class="p-6 bg-white">
+                <h1 class="pb-2 font-semibold text-green-500">
+                    2020 - 2021
+                </h1>
+                <p class="text-sm">
+                    NU Care-LAZISNU melakukan penguatan system dan regulasi,      penggunaan system informasi manajemen ZIS yang dalam pengelolaan manajemen ZIS di internal NU Care Lazisnu Cilacap, perencanaan dan pembuatan      aplikasi GOCAP yang resmi milik NU Care Lazisnu Cilacap dan melakukan sosialisasi Pedoman Organisasi serta meluncurkan 4 Pilar Program Kemanusiaan (Ekonomi, Pendidikan, Kesehatan, Kebencanaan)., NU Care Lazisnu Cilacap      juga melakukan beberapa pengembangan dan penguatan program antara lain,      (1) penyusunan regulasi manajemen ekskeutif (2) konsolidasi dan koordinasi      jaringan NU Care Lazisnu Cilacap (3) menjalin kerjasama lintas Lembaga,      banom NU dan corporate (4) pengembangan system Melakukan integrasi      database muzakki dan mustahiq; (5) penignkatan penghimpunan dana Koin NU      Cilacap dan optimalisasi penghimpunan ZIS secara digital melalui website      crowdfunding; (6) penataan manajemen eksekutif dan pengangkatan staf untuk      divisi -divisi di manajemen eksekutif LAZISNU PCNU Cilacap (7) pengurusan      ijin operasional NU Care Lazisnu Cilacap periode 2020-2022,
+                </p>
+            </div>
+            <div class="p-6 bg-white">
+                <h1 class="pb-2 font-semibold text-green-500">
+                    2022 - Juni 2023
+                </h1>
+                <p class="text-sm">
+                    NU Care Lazisnu Cilacap terus melakukan berbagai pengembangan dan penguatan program untuk "tinggal landas", seperti di      antaranya, (1) pengurusan ijin operasional NU Care Lazisnu Cilacap untuk      periode 2022-2024 (2) Pertumbuhan pengelolaan dana ZIS dan DSKL mencapai rasio optimis 80-100%; (3) Penguatan kaderisasi amil di 80 titik di Indonesia; (4) Melakukan training kompetensi amil zakat di lanjutkan      dengan uji kompetensi amil untuk 3 bidang pengelolaan zakat yang      bekerjasama dengan LSP BEKSYA; (5) Melakukan pelayanan mustahiq di 24      Kecamatan di wilayah Kabupaten Cilacap (6) NU Care Lazisnu Cilacap  menjadi percontohan penghimpunan dan      pengelolaan ZIS bagi berbagai LAZ, LAZISNU Cilacap juga menjadi narasumber      di berbagai daerah baik PW LAZISNU / PC LAZISNU serta banyaknya PC LAZISNU      yang melakukan study banding / silaturahim dengan lebih dari 30 daerah      yang dating ataupun yang di isi oleh NU Care Lazisnu Cilacap; (7)      Terlaksananya Program “KOIN NU Cilacap di 261 ranting; (8) 80% amil LAZISNU      Cilacap tersertifikasi; (9) Memiliki 1 gedung kantor wilayah yang      representative  ; (10) Penguatan      sistem digital dalam pengelolaan ZIS dan DSKL; (11) Terlibat aktif dalam      pembentukan dan penanganan pandemi dalam Tim Satgas NU Peduli Covid-19.      (12) memiliki 1 perahu ambulance, 1 perahu dakwah, 1 mobil rescue, 1 mobil      klinik dan 17 ambulance. (12) Mengoptimalkan penghimpunan dana ZIS secara      digital melalui website crowdfunding dna pembuatan tim digital fundraising      (13) menjalin kerjasama dengan corporate melalui penyaluran dana CSR      seperti Perumda Kawasan Industri Cilacap, PT.S2P PLTU Karangkandri      Cilacap, PT.Indonesia Power PLTU Adipala, dan lain banyak lagi lainnya      (13) menjalin kerjasama dengan BMT dan BRI Cilacap dalam program Koin NU      Cilacap (14) berbagai penghargaan juga telah diraih oleh NU Care Lazisnu      Cilacap seperti penghargaan sebagai LAZ Progresif dan dinamis dari Kantor      Kementerian Agama Kabupaten Cilacap, penghargaan sebagai LAZ terbaik dan      responsive terhadap pelayanan umat dari Kantor Kementerian Agama Kabupaten      Cilacap, penghargaan sebagai inisiator digitalisasi Koin NU oleh Pengurus      Pusat LAZISNU, penghargaan sebagai LAZ teraktif dalam pencegahan dan      penanganan pandemi Covid-19 di wilayah Kabupaten Cilacap dari Dinas      Kesehatan Kabupaten Cilacap, penghargaan sebagai mitra polres Cilacap      dalam program vaksinasi Covid-19 guna penanggulangan pencegahan Covid-19      di wilayah Kabupaten Cilacap, serta masih banyak lagi penghargaan, program      program dan kinerja yang di lakukan oleh NU Care Lazisnu Cilacap.
+                </p>
+                <p class="pt-2 text-sm">
+                    Sampai saat ini, NU Care Lazisnu Cilacap  telah memiliki jaringan Gerakan Koin NU Cilacap (GOCAP), di 282 Ranting (Desa/Kelurahan), 23 UPZIS MWCNU (Kecamatan) dan 2.260 orang penggerak Koin NU Cilacap. NU Care Lazisnu Cilacap sebagai lembaga filantropi akan terus berupaya untuk meningkatkan kepercayaan dari para donatur yang semua sistem pencatatan dan penyalurannya disampaikan secara akuntabel, transparan, amanah, profesional.
+                </p>
+            </div>
+        </div>
+        <div class="mb-16"></div>
+    </div>
+</div>
