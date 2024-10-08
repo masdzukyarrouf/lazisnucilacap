66b8dc8f52d6f437a0a0579a7c0f05377ee5581e
AndikaBastian <andikabastian20@gmail.com>
2024-10-08 14:36:24 +0700

    Update nav-mobile2.blade.php

--- resources/views/components/nav-mobile2.blade.php
+++ resources/views/components/nav-mobile2.blade.php
@@ -1,4 +1,4 @@
-<div class="sticky inset-0 top-0 z-10 flex items-center w-full h-16 bg-green-500">
+<div class="sticky inset-0 top-0 z-10 flex items-center md:w-[414px] h-16 bg-green-500 w-full">
     <div class="flex items-center pl-5">
         <a href="{{ $backUrl ?? 'javascript:history.back()' }}">
             <img src="{{ asset('images/backArrow.png') }}" alt="back button" class="w-6 h-auto">
