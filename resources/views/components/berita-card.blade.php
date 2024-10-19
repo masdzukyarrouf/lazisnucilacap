c6150194556c35f982cf67490cda292a871edf91
Mas Dzuky Ar-Rouf <127199885+Einkelberg@users.noreply.github.com>
2024-10-19 09:32:47 +0700

    fix : berita card

--- resources/views/components/berita-card.blade.php
+++ resources/views/components/berita-card.blade.php
@@ -1,19 +1,26 @@
-<div class="w-full h-[130px] mb-4 bg-white pt-2 z-5 md:flex-col bg-black">
-    <a href="{{ route('user-berita.show', $berita->title_berita) }}"
-       class="flex flex-grow text-sm text-green-500 md:flex-col md:flex md:text-base hover:text-green-600 hover:cursor-pointer">
-        <div class="relative z-0 flex items-center w-full h-full group md:h-80 md:w-auto">
+<div class="z-5 flex flex-grow bg-white h-[130px] md:h-auto md:flex-col">
+    <a href="{{ route('user-berita.show', $berita->title_berita) }}" class="flex flex-grow w-[280px] h-[130px] md:w-auto md:h-[500px] md:flex-col">
+        <!-- Image Section -->
+        <div class="z-0 relative group flex justify-center items-center w-[280px] h-[130px] md:w-full md:h-[430px] overflow-hidden">
             <img src="{{ asset('storage/' . $berita->picture) }}" alt="Picture"
-                 class="object-cover w-[180px] h-[130px] hover:cursor-pointer">
+                class="object-cover w-full h-full hover:cursor-pointer">
         </div>
-        <div class="w-full px-3 py-2 md:p-4">
-            <h2 class="text-sm font-semibold text-gray-800 md:text-xl">
-                {{ \Illuminate\Support\Str::limit($berita->title_berita, 25, '...') }}
+        
+        <!-- Text Section -->
+        <div class="py-2 px-3 w-4/5 flex flex-col justify-between md:w-full">
+            <h2 class="text-[14px] md:text-lg font-semibold text-gray-800">
+                {{ \Illuminate\Support\Str::limit($berita->title_berita, 35, '...') }}
             </h2>
-            <div class="flex items-center mt-1">
-                <img src="{{ asset('images/clock.png') }}" alt="clock" class="w-3 h-3">
-                <p class="pl-1 text-xs text-gray-600 md:text-sm">{{$berita->tanggal}}</p>
+            <div class="flex justify-between items-center mt-2 md:text-lg">
+                <div class="flex items-center">
+                    <img src="{{ asset('images/clock.png') }}" alt="clock" class="w-3 h-3">
+                    <p class="pl-1 text-xs text-gray-600 md:text-sm">{{ $berita->tanggal }}</p>
+                </div>
             </div>
-            Baca Selengkapnya ...
+            <p class="pl-1 text-[12px] md:text-lg text-left text-green-600">{{ $berita->kategori->nama_kategori ?? '' }}</p>
+            <div class="mt-3">
+                <p class="text-[12px]  md:text-lg text-green-600"> Baca Selengkapnya...</p>
+            </div>
         </div>
     </a>
 </div>
